5d9d1c6ef4d8939ec6f536b1a611b88640ece178
quantsegu <segulavs@hotmail.com>
2020-05-29 08:11:28 +0200

    Update ClassExtractT-SQLCodeAndSave.py

--- ClassExtractT-SQLCodeAndSave.py
+++ ClassExtractT-SQLCodeAndSave.py
@@ -28,3 +28,27 @@ class ScriptDownloader:
 
         finally:
             cnxn.close()
+            
+#
+#  ------------------------------------------- Stored Procedures  ---------------------------------------------
+# SELECT  s.name SchemaName
+#       ,o.name FunctionName
+#       ,  sm.definition  
+# FROM sys.sql_modules AS sm 
+# JOIN sys.objects AS o 
+# ON sm.object_id = o.object_id  
+# left join sys.schemas s 
+# on s.schema_id = o.schema_id
+# WHERE o.name like 'usp%'
+# ------------------------------------------- Functions---------------------------------------------------------
+# y = x.FetchObjectsAndStore('''SELECT  s.name SchemaName
+#       ,o.name FunctionName
+#       ,  sm.definition  
+# FROM sys.sql_modules AS sm 
+# JOIN sys.objects AS o 
+# ON sm.object_id = o.object_id  
+# left join sys.schemas s 
+# on s.schema_id = o.schema_id
+# WHERE o.name like \'uf%\''''
+# ------------------------------------------- Views---------------------------------------------------------
+#y = x.FetchObjectsAndStore("select s.name as SchemaName,v.name as ViewName,definition from sys.objects     o join sys.sql_modules m on m.object_id = o.object_id join sys.views v on o.object_id = v.object_id join sys.schemas s on v.schema_id = s.schema_id where o.type      = 'V' and s.name = 'dbo' order by 1")
