6724f4e285270728edd4d49f996c7c57fca0c1d1
Lakshmi Segu <segulavs@hotmail.com>
2020-06-14 14:55:21 +0200

    Make Eikon Code parametrized

--- Eikon/eikoncdstest.py
+++ Eikon/eikoncdstest.py
@@ -4,21 +4,18 @@ import pandas as pd  # pandas
 import configparser as cp
 import warnings
 import sys
-warnings.filterwarnings("ignore")
+# underlying use case
 
-ek.set_app_id('a17f5ab013e449cc86857ecbfe62f4c96577df86')
-df = pd.read_csv(r'C:\Users\segul\OneDrive\Documents\CDSNLTickers.csv', header=None)
+warnings.filterwarnings("ignore")
+df = pd.read_csv(r'C:\Users\segul\OneDrive\Documents\ReutersTickers.csv', header=None)
+ek.set_app_key('a17f5ab013e449cc86857ecbfe62f4c96577df86')
 tickers = list(df[0])
-d = []
-dates = ['2020-03-02', '2020-03-16', '2020-04-10']
-for x in dates:
-    data,error = ek.get_data(tickers,
-                   ['TR.PARMIDSPREAD'],
-                   {'SDate': x, 'EDate': x, 'DateType': 'AD', 'CURN': 'EUR'})
-    data.dropna(inplace=True)
-    data['Date'] = x
-    d.append(data)
-FinalData = pd.concat(d)
-FinalData.to_csv(r'C:\Users\segul\OneDrive\Documents\CDSNLTickersResult.csv')
-# print(tickers)
-print('#1')
+data, error = ek.get_data(tickers,'TR.CDSPrimaryCDSRic')
+data.dropna(inplace=True)
+data = data[data['Primary CDS RIC']!='']
+data.to_csv(r'C:\Users\segul\OneDrive\Documents\ReutersTickersData.csv')
+cdsIdList = [str(cdsId)  for cdsId in data.ix[:,1].values]
+result, err = ek.get_data(cdsIdList, ['PRIMACT_1'])
+result.to_csv(r'C:\Users\segul\OneDrive\Documents\ReutersTickersOutput2.csv')
+dfFinal = pd.merge(data, result, left_on='Primary CDS RIC', right_on='Instrument', how='left')
+dfFinal.to_csv(r'C:\Users\segul\OneDrive\Documents\ReutersTickersFinal.csv')
--- Eikon/eikontest.py
+++ Eikon/eikontest.py
@@ -1,23 +1,30 @@
 import eikon as ek  # the Eikon Python wrapper package
 import numpy as np  # NumPy
 import pandas as pd  # pandas
-import configparser as cp
 import warnings
 import sys
+import json
+import os
+
+with open(os.path.dirname(os.path.realpath(__file__)) + '\\config.json', 'r') as f:
+    config = json.load(f)
+
+# Get information from the Configuration file
+Tickers = config['TickersFile']
+ReutersKey = config['ReutersKey']
+FieldsToFetch = config['FieldsToFetch']
+Options = config["FetchOptions"]
+
+options = ""
+
+for attribute, value  in Options.items():
+    options = options + '\'' + attribute + '\':\'' + value + '\','
+
 warnings.filterwarnings("ignore")
-df = pd.read_csv(r'C:\Users\segul\OneDrive\Documents\ReutersTickers.csv', header=None)
-ek.set_app_id('a17f5ab013e449cc86857ecbfe62f4c96577df86')
+
+df = pd.read_csv(Tickers, header=None)
+ek.set_app_key(ReutersKey)
+
 tickers = list(df[0])
-# text = ''
-data, error = ek.get_data(tickers,'TR.CDSPrimaryCDSRic')
-data.dropna(inplace=True)
-data = data[data['Primary CDS RIC']!='']
-data.to_csv(r'C:\Users\segul\OneDrive\Documents\ReutersTickersData.csv')
-cdsIdList = [str(cdsId)  for cdsId in data.ix[:,1].values]
-print('================')
-result, err = ek.get_data(cdsIdList, ['PRIMACT_1'])
-print('#2')
-result.to_csv(r'C:\Users\segul\OneDrive\Documents\ReutersTickersOutput2.csv')
-dfFinal = pd.merge(data, result, left_on='Primary CDS RIC', right_on='Instrument', how='left')
-dfFinal.to_csv(r'C:\Users\segul\OneDrive\Documents\ReutersTickersFinal.csv')
-print('#3')
+data, error = ek.get_data(tickers,FieldsToFetch,options[:-1])
+data.to_csv(str(Tickers).replace('.csv', '.result'))
